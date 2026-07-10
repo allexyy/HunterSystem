66c79c6db0f374e402a3db8bb89e93085cffac22
alexyy <lillexlilflex@gmail.com>
2026-07-10 18:26:47 +0300

    stats

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,3 +1,15 @@
 package user
 
-//TODO: Make model to devide
+type User struct {
+	Name  string
+	Level int32
+	Stats []UserStats
+	Xp    int64
+	Gold  int64
+	Rank  string
+}
+
+type UserStats struct {
+	Name  string
+	Value int32
+}
